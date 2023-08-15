556c571b79cb7e565f529ac91b178b0b5d8d10bb
JayChandlani <jaychandlanijay@gmail.com>
2023-08-15 17:34:44 +0530

    added code comments

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ const express = require('express');
 const app = express()
 const subscribers = require('./models/subscribers');
 const path = require('path')
-// Your code goes here
 
+// Renders the documentation view
 app.get('/', async (req, res) => {
     try {
         res.sendFile(path.join(__dirname + '/views/index.html'))
@@ -12,6 +12,8 @@ app.get('/', async (req, res) => {
         res.status(404).json({ message: error.message });
     }
 })
+
+// Returns an array of all subscribers in the database.
 app.get('/subscribers', async (req, res) => {
     try {
         const data = await subscribers.find({});
@@ -20,6 +22,8 @@ app.get('/subscribers', async (req, res) => {
         res.status(404).json({ message: error.message });
     }
 })
+
+// Returns an array of all subscribers' names and subscribed channels in the database.
 app.get('/subscribers/names', async (req, res) => {
     try {
         const data = await subscribers.find({}, { _id: 0, name: 1, subscribedChannel: 1 });
@@ -28,6 +32,8 @@ app.get('/subscribers/names', async (req, res) => {
         res.status(404).json({ message: error.message });
     }
 })
+
+// Returns the subscriber with the specified ID.
 app.get('/subscribers/:id', async (req, res) => {
     const { id } = req.params;
     try {
