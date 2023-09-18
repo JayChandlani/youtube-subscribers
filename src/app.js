79cd9ae2ef147e11ca7b9d1861573d838ab44fbe
JayChandlani <jaychandlanijay@gmail.com>
2023-09-18 12:00:18 +0530

    updated code

--- src/app.js
+++ src/app.js
@@ -1,47 +1,53 @@
-
-const express = require('express');
-const app = express()
-const subscribers = require('./models/subscribers');
-const path = require('path')
+const express = require("express");
+const app = express();
+const subscribers = require("./models/subscribers");
+const path = require("path");
 
 // Renders the documentation view
-app.get('/', async (req, res) => {
-    try {
-        res.sendFile(path.join(__dirname + '/views/index.html'))
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-})
+app.get("/", async (req, res) => {
+  try {
+    res.sendFile(path.join(__dirname + "/views/index.html"));
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
 
 // Returns an array of all subscribers in the database.
-app.get('/subscribers', async (req, res) => {
-    try {
-        const data = await subscribers.find({});
-        res.status(200).json(data)
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-})
+app.get("/subscribers", async (req, res) => {
+  try {
+    const data = await subscribers.find({});
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
 
 // Returns an array of all subscribers' names and subscribed channels in the database.
-app.get('/subscribers/names', async (req, res) => {
-    try {
-        const data = await subscribers.find({}, { _id: 0, name: 1, subscribedChannel: 1 });
-        res.status(200).json(data)
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-})
+app.get("/subscribers/names", async (req, res) => {
+  try {
+    const data = await subscribers.find(
+      {},
+      { _id: 0, name: 1, subscribedChannel: 1 }
+    );
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
 
 // Returns the subscriber with the specified ID.
-app.get('/subscribers/:id', async (req, res) => {
-    const { id } = req.params;
-    try {
-        const data = await subscribers.findById(id);
-        res.status(200).json(data)
-    } catch (error) {
-        res.status(404).json({ message: error.message });
+app.get("/subscribers/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await subscribers.findById(id);
+    if (data) {
+      res.status(200).json(data);
+    } else {
+      res.status(404).json({ message: "no data found for this id please provide a valid id" });
     }
-})
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
 
 module.exports = app;
